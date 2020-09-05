b791c42ca31e68cee4ac6692060f93b7be1d0eda
Chris Chang <chris@crchang.com>
2020-09-05 14:34:39 -0700

    Reorganized code + initial RE checking

--- cfg.py
+++ cfg.py
@@ -1,4 +1,7 @@
 
-DATA_DIR = 'data'
-OUT_DIR = 'out'
+DATA_DIR = 'data/'
+OUT_DIR = 'out//'
 RESTRICTSITES_FN = DATA_DIR+'/restriction_sites.fasta'
+
+OLIGO_CHECK_PRE = 'CACTGC'
+OLIGO_CHECK_POST = 'AGAGGG'
